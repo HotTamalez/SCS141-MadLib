7c96c5b9a5d9e32b2b3cf49ed6ed8fb34a6d3605
Hot Tamale <zweakland@setonhill.edu>
2021-03-08 17:44:06 -0500

    Final version

--- madLib/ViewController.swift
+++ madLib/ViewController.swift
@@ -9,6 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputField: UITextField!
+    @IBOutlet weak var outputField: UITextField!
+    
+    var sentence : String = "Hi my name is <name> and I just want warm weather again."
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        // First Example
+        // outputField.text = inputField.text
+        
+        //Second Example
+        // replace <name> in the sentence variable
+        // with the input from the user
+        outputField.text = sentence.replacingOccurrences(of: "<name>", with: inputField.text!)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
